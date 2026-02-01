0ef28cd5133ecb30e0318cc4abbbd4e9f957fdbd
Anthony-Albuquerque405 <anthonyhitnailbr@gmail.com>
2026-02-01 08:46:40 -0300

    feat: aprimorar componente Switch com ícones e corrigir estilo

--- src/components/ButtonDarkMod.tsx
+++ src/components/ButtonDarkMod.tsx
@@ -1,23 +1,36 @@
 "use client";
 
-import { useState } from "react";
-import { Button } from "./ui/button";
+import { useEffect, useState } from "react";
 import { Switch } from "./ui/switch";
+import { Sun, Moon } from "lucide-react";
 
 const ButtonDarkMod = () => {
   const [darkMod, setDarkMod] = useState(false);
 
+  useEffect(() => {
+    const saveThema = localStorage.getItem("theme");
+    if (saveThema === "dark") {
+      document.querySelector("html")?.classList.add("dark");
+      setDarkMod(true);
+    } else {
+      document.querySelector("html")?.classList.remove("dark");
+      setDarkMod(false);
+    }
+  }, []);
+
   const togleeDakMod = () => {
-    document.querySelector("html")?.classList.add("dark");
+    document.querySelector("html");
     if (darkMod) {
       document.querySelector("html")?.classList.remove("dark");
+      localStorage.setItem("theme", "light");
       setDarkMod(false);
     } else {
       document.querySelector("html")?.classList.add("dark");
+      localStorage.setItem("theme", "dark");
       setDarkMod(true);
     }
   };
-  return <Switch onClick={() => togleeDakMod()}>Dark-Mod</Switch>;
+  return <Switch checked={darkMod} onCheckedChange={togleeDakMod}></Switch>;
 };
 
 export default ButtonDarkMod;
