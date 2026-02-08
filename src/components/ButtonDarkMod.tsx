3e2c479f587619b94ed551e699e47dc2652322c0
Anthony-Albuquerque405 <anthonyhitnailbr@gmail.com>
2026-02-08 14:48:45 -0300

    feat: corrigir cor do ícone da lua no componente ButtonDarkMod

--- src/components/ButtonDarkMod.tsx
+++ src/components/ButtonDarkMod.tsx
@@ -39,7 +39,7 @@ const ButtonDarkMod = () => {
         className="data-[state=checked]:bg-blue-700 data-[state=unchecked]:bg-yellow-400 
                  relative inline-flex h-6 w-12 items-center rounded-full transition-colors duration-300"
       ></Switch>
-      <Moon className="text-blue-400 transition-transform duration-300 hover:scale-110" />
+      <Moon className="text-red-400 transition-transform duration-300 hover:scale-110" />
     </div>
   );
 };
